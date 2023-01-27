cb25b8890097fac2a0faac2824df2d281bfcee62
Arnaud Nicastro <arnaud.nicastro@gmail.com>
2023-01-27 19:07:34 +0100

    Style index.html

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,59 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
+    console.log(city);
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        // article.classList.add('photographer-card');
+
+        // Ajout de lien
+        const link = document.createElement('a');
+        link.href = '../../photographer.html?id=' + id;
+        link.setAttribute("aria-label", "Lien vers la page de " + name);
+        // link.classList.add('photographer-card-link');
+
+        // Ajout de l'image
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", "Photo de " + name);
+        // img.classList.add('photographer-card-avatar');
+
+        //Ajout du nom
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        h2.setAttribute("aria-label", "Nom du photographe");
+        // h2.classList.add('photographer-card-name');
+
+        //Ajout de la city
+        const cityElement = document.createElement( 'h3' );
+        cityElement.textContent = `${city}, ${country}`;
+        cityElement.setAttribute("aria-label", "Ville et pays du photographe");
+        // cityElement.classList.add('photographer-card-city');
+
+        //Ajout de la tagline
+        const taglineElement = document.createElement( 'p' );
+        taglineElement.textContent = tagline;
+        taglineElement.setAttribute("aria-label", "Tag line");
+        // taglineElement.classList.add('photographer-card-tagline');
+
+        //Ajout du TJM
+        const priceElement = document.createElement( 'div' );
+        priceElement.textContent = `${price}€/jour`;
+        priceElement.setAttribute("aria-label", "Prix par jour");
+        // priceElement.classList.add('photographer-card-price');
+
+        //Constructeur fiche photographe
+        article.appendChild(link);
+        link.appendChild(img);
+        link.appendChild(h2);
+        // article.appendChild(img);
+        // article.appendChild(h2);
+        article.appendChild(cityElement);
+        article.appendChild(taglineElement);
+        article.appendChild(priceElement);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,7 +1,7 @@
        async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        return  fetch('./data/photographers.json')
+        return fetch('./data/photographers.json')
 
         .then(function(res) {
             console.log(res);
