18b5f23dcc63784a12cbbb97353177eb9b450a0d
Arnaud Nicastro <arnaud.nicastro@gmail.com>
2023-02-07 16:50:57 +0100

    MediaFactory

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -3,11 +3,52 @@ function mediaFactory(data) {
 
   const picture = `./assets/images/${image}`;
   const _video = `./assets/images/${video}`;
-  const titre = `./assets/images/${title}`;
+  const titre = title;
   const coeur = `./assets/images/${likes}`;
 
   function getMediaCardDOM() {
-    console.log("Tout les médias");
+    console.log(data);
+    //Création de la div
+    const div = document.createElement('div');
+    div.setAttribute('class', 'media');
+
+    if ('video' in data) {
+      console.log('video');
+      //Création de la balise video
+      const video = document.createElement('video');
+      video.setAttribute('controls', 'controls');
+      // video.setAttribute('class', 'video');
+
+      //Création de la balise source
+      const source = document.createElement('source');
+      source.setAttribute('src', _video);
+      source.setAttribute('alt', titre);
+
+      const titreVideo = document.createElement('p');
+      titreVideo.setAttribute('class', 'titre');
+      titreVideo.textContent = titre;
+      //Ajout de la balise source dans la balise video
+      video.appendChild(source);
+      div.appendChild(titreVideo);
+      //Ajout de la balise video dans la div
+      div.appendChild(video);
+
+    } else {
+
+      console.log('image');
+      //Création de la balise img
+      const img = document.createElement('img');
+      // img.setAttribute('class', 'photo');
+      img.setAttribute('src', picture);
+      img.setAttribute('alt', titre);
+      //Ajout de la balise img dans la div
+      const titreVideo = document.createElement('p');
+      titreVideo.setAttribute('class', 'titre');
+      titreVideo.textContent = titre;
+      div.appendChild(img);
+      div.appendChild(titreVideo);
+    }
+    return div;
   }
   return { titre, coeur, _video, picture, date, getMediaCardDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -19,17 +19,27 @@ async function displayData(photographers, media) {
   const filteredPhotographers = photographers.filter((obj) => obj.id == _id)
   const photographersSection = document.querySelector('.photograph-header')
 
-  filteredPhotographers.forEach((photographer) => {
-    const photographerModel = photographerFactory(photographer)
-    const userPicture = photographerModel.getUserPicture()
-    photographersSection.appendChild(userPicture)
-    const userInfo = photographerModel.getUserInfo()
-    const infoPrice = document.querySelector('.infos__price')
-    photographersSection.appendChild(userInfo)
-    const userPriceDOM = photographerModel.getUserPriceDom()
-    infoPrice.appendChild(userPriceDOM)
 
+  filteredPhotographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userPicture = photographerModel.getUserPicture();
+    photographersSection.appendChild(userPicture);
+    const userInfo = photographerModel.getUserInfo();
+    const infoPrice = document.querySelector('.infos__price');
+    photographersSection.appendChild(userInfo);
+    const userPriceDOM = photographerModel.getUserPriceDom();
+    infoPrice.appendChild(userPriceDOM);
   })
+
+  const filteredMedias = media.filter((obj) => obj.photographerId == _id)
+  console.log(filteredMedias)
+  const mediaSection = document.getElementsByClassName('gallery');
+  console.log(mediaSection)
+  filteredMedias.forEach((media) => {
+    const mediaModel = mediaFactory(media);
+    const mediaCard = mediaModel.getMediaCardDOM();
+    mediaSection[0].appendChild(mediaCard);
+  });
 }
 
 async function init() {
